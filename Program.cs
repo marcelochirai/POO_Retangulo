803e3c95ddb99d06111c7418c3aff74c508fcc2c
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-09-15 01:02:28 -0300

    Add files via upload
    Cálculo da área, perímetro e diagonal do retângulo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Retangulo;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle x;
+
+            x = new Rectangle();
+            
+            Console.WriteLine("Informe as medidas da altura e largura do retângulo: ");
+            x.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Retangulo
+{
+    class Rectangle
+    {
+		//Definição das variáveis, lados 
+		public double Height;
+		public double Width;
+		
+		//Método de cálculo da área
+		public double Area()
+		{
+			double areaRet = Height*Width;
+			return areaRet;
+		}
+
+		//Método de cáclulo do perímetro
+		public double Perimeter()
+        {
+			double periRet = 2 * Height + 2 * Width;
+			return periRet;
+        }
+
+		//Método de cálculo da diagonal
+		public double Diagonal()
+		{
+			return Math.Sqrt(Width * Width + Height * Height);
+		}
+
+		//Método ToString para inserir todas as informações unificadas
+		public override string ToString()
+		{
+			return "Perímetro: "+Perimeter().ToString("F3", CultureInfo.InvariantCulture)
+			+ " Área: "+ Area().ToString("F3", CultureInfo.InvariantCulture)
+			+ " Diagonal: "+Diagonal().ToString("F3", CultureInfo.InvariantCulture);
+
+		}
+
+	}
+}
+
